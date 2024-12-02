e1485bd03230bcf9ed23d0abe89604301f896ec9
kaushalsisat@gmail.com <kaushalsisat@gmail.com>
2024-12-03 01:09:44 +0530

    changed by Kaushal

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
       xl:"1440px"
     },
     extend: {
-      // additional custom styles if required
+      colors: {
+        heroBg: "#0D0D16", // Custom color for hero background
+        primaryColor: "#9A999E",
+        secondaryColor: "#522699",
+        starColor: "#FFDF00",
+      },
     },
   },
   plugins: [],
