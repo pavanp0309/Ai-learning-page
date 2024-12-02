1818041db8bdd140744f31710c77a349fb053d8c
Vivekmoyaje <vivekmoyaje@gmail.com>
2024-12-02 21:49:17 +0530

    new file added by vivek

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
     },
     extend: {
       // additional custom styles if required
+      colors: {
+        heroBg: "#0D0D16", // Custom color for hero background
+        primaryColor: "#9A999E",
+        secondaryColor: "#522699",
+        starColor: "#FFDF00",
+      },
     },
   },
   plugins: [],
